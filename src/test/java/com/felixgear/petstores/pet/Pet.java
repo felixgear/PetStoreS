811837f6e39098606a7f5f47fe9db7f8f6e92919
Andrási Alex (Félix) <26480791+felixgear@users.noreply.github.com>
2024-06-18 13:21:13 +0200

    Pts 22 fix/test output dependencies (#4)
    * PTS-22 Remove Extent Report files
    
    * PTS-22 Exclude Extent Report folders
    
    * PTS-22 Add the right jaxb dependency for json deserialization
    
    * PTS-22 Fix pet deserialization

--- src/test/java/com/felixgear/petstores/pet/Pet.java
+++ src/test/java/com/felixgear/petstores/pet/Pet.java
@@ -1,19 +1,10 @@
 package com.felixgear.petstores.pet;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Pet {
 
